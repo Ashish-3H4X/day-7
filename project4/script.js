6324e80cd05c43a50b910206761469297375801a
Mr Rahul <ashish227087@gmail.com>
2025-04-07 22:19:13 +0530

    Display the value using loops

---
+++ project4/script.js
@@ -0,0 +1,17 @@
+// Create an Object
+const person = {
+    name: "John",
+    age: 30,
+    city: "New York"
+  };
+  
+  // Build a Text
+  let text = "";
+  for (let x in person) {
+    text += person[x]+"";
+    
+    // text += person[x] + " ";
+  };
+  
+  // Display the Text
+  document.getElementById("demo").innerHTML = text;
