e4c11df072dddeaff3d9436472beeaadc6c1f7dc
Mr Rahul <ashish227087@gmail.com>
2025-04-07 11:42:33 +0530

    Creating an object with new  keyword and  push the value

--- project1/script.js
+++ project1/script.js
@@ -32,3 +32,26 @@ demo3.innerHTML = car.color;
 emptyDemo1.innerHTML= person.name;
 emptyDemo2.innerHTML=person.age;
 emptyDemo3.innerHTML= person.eyeColor;
+
+
+// creating the  object using new  key word 
+
+ const student = new Object()
+ // adding properties
+  student.firstName ="Ashish";
+  student.lastName ="Verma";
+  student.age =21;
+  student.eyeColor = "blue"
+
+  newdemo.innerHTML= ` const student = new Object()<br>
+ // adding properties<br>
+  student.firstName ="Ashish";<br>
+  student.lastName ="Verma";<br>
+  student.age =21;<br>
+  student.eyeColor = "blue"<br>
+  Result: <br>
+`
+   newdemo1.innerHTML=student.firstName;
+   newdemo2.innerHTML=student.lastName;
+   newdemo3.innerHTML=student.age;
+   newdemo4.innerHTML=student.eyeColor;
