c0ad8d28fcb063385ad3862868afe1af62eaca77
Mr Rahul <ashish227087@gmail.com>
2025-04-07 11:05:54 +0530

    JavaScript Object

---
+++ project1/script.js
@@ -0,0 +1,14 @@
+const car = {
+    name: "fiat",
+    model: 500,
+    color: "white"
+}
+demo.innerHTML = `  const car = {
+  name:"fiat",<br>
+ model: 500,<br>
+ color: "white"<br>
+ Result : <br>
+  }`
+demo1.innerHTML = car.name;
+demo2.innerHTML = car.model;
+demo3.innerHTML = car.color;
