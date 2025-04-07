d6cbcc61325b8826626976897915552a809ad2d6
Mr Rahul <ashish227087@gmail.com>
2025-04-07 11:12:18 +0530

    creating empty object and push the value

--- project1/script.js
+++ project1/script.js
@@ -12,3 +12,23 @@ demo.innerHTML = `  const car = {
 demo1.innerHTML = car.name;
 demo2.innerHTML = car.model;
 demo3.innerHTML = car.color;
+
+
+ 
+
+ const person ={};
+
+  person.name ="Rahul";
+  person.age=21;
+  person.eyeColor="blue"
+
+  emptyDemo.innerHTML=` const person ={};<br>
+  person.name ="Rahul";<br>
+  person.age=21;<br>
+  person.eyeColor="blue"<br>
+  <br>
+  Result :<br>
+`
+emptyDemo1.innerHTML= person.name;
+emptyDemo2.innerHTML=person.age;
+emptyDemo3.innerHTML= person.eyeColor;
