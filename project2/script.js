f793dd8d204f61ba132843000e5397457c9c3759
Mr Rahul <ashish227087@gmail.com>
2025-04-07 21:33:27 +0530

    nested Object

--- project2/script.js
+++ project2/script.js
@@ -15,3 +15,19 @@ objectpr2.innerHTML= x
   delete person.age;
 
    console.log(person);
+
+   /// nested object 
+
+   myObj = {
+    name:"John",
+    age:30,
+    myCars: {
+      car1:"Ford",
+      car2:"BMW",
+      car3:"Fiat"
+    }
+  }
+    console.log(myObj.myCars.car1);
+    console.log(myObj.myCars.car2);
+    console.log(myObj.myCars.car3);
+    
